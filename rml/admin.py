f00e81a368a0c4c59afea02b7e76e32a047fece9
Vishal <shahivishal09@gmail.com>
2016-07-28 23:40:52 +0530

    Entry added via admin

--- rml/admin.py
+++ rml/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import InputDetails
 # Register your models here.
+
+admin.site.register(InputDetails)
--- rml/models.py
+++ rml/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class InputDetails(models.Model):
+	email = models.CharField(max_length=100)
+	phone = models.IntegerField()
+	preferred = models.CharField(max_length=100)
+	reminder = models.DateTimeField()
